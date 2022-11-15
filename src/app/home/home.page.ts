6558fdbb74aec194042eedd80fc8331ea11dab3b
PremanandChowdhury <freudigprem@gmail.com>
2022-11-16 00:07:18 +0530

    Fetching the extenal data and modifying home page

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit {
+  categories: any[] = [];
+  featured: any[] = [];
+  highlights: any[] = [];
 
-  constructor() {}
+  constructor(private http: HttpClient) {}
 
+  ngOnInit() {
+    this.http
+      .get('https://devdactic.fra1.digitaloceanspaces.com/foodui/home.json')
+      .subscribe((result: any) => {
+        this.categories = result.categories;
+        this.featured = result.featured;
+        this.highlights = result.hightlights;
+      });
+  }
 }
